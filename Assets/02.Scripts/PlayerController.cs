0a7242afe494d5ec2f6f84ce8087703c4a1f6651
An-Shine <akh1012@naver.com>
2024-08-31 13:07:38 +0900

    ssss

---
+++ Assets/02.Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    Animator animator;
+
+    float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+
+    void Start()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Á¡ÇÁÄÚµå
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid2D.AddForce(transform.up * jumpForce);
+        }
+
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            key = 1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            key = -1;
+        }
+        float speedX = Mathf.Abs(rigid2D.velocity.x);
+
+        if (speedX < maxWalkSpeed)
+        {
+            rigid2D.AddForce(transform.right * key * walkForce);
+        }
+
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        animator.speed = speedX / 2.0f;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("°ñ");
+    }
+
+}
+
+
